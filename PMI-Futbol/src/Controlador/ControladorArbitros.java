ae89edb7b31564d735df97b0e9ed9a5248f710c2
Ignacio Arr <68046735+IgnacioArrieta@users.noreply.github.com>
2025-05-16 19:05:14 -0300

    Controlador de Arbitros
    tengo que ver como hacerlo, pero ahora no lo puedo terminar

---
+++ PMI-Futbol/src/Controlador/ControladorArbitros.java
@@ -0,0 +1,15 @@
+
+package Controlador;
+
+import Model.Arbitro;
+import java.util.ArrayList;
+
+public class ControladorArbitros {
+    
+        //Variables de instancia
+
+    private Arbitro arbitro = new Arbitro();
+    private ArrayList<Arbitro> listaArbitros =  new ArrayList<Arbitro>();
+
+
+}
