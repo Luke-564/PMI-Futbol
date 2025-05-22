2d0c0fa69d1917146a8d851c6c5b1c8d030f40fd
Ignacio Arr <68046735+IgnacioArrieta@users.noreply.github.com>
2025-05-22 15:57:47 -0300

    aCTUALIUZAAO

--- PMI-Futbol/src/Controlador/ControladorArbitros.java
+++ PMI-Futbol/src/Controlador/ControladorArbitros.java
@@ -8,8 +8,73 @@ public class ControladorArbitros {
     
         //Variables de instancia
 
-    private Arbitro arbitro = new Arbitro();
-    private ArrayList<Arbitro> listaArbitros =  new ArrayList<Arbitro>();
+    private final Arbitro arbitro = new Arbitro();
+    private final ArrayList<Arbitro> listaArbitros;
+    private final ArrayList<Arbitro> listaTempArbitros;
+
+    public ControladorArbitros(){
+        this.listaArbitros = new ArrayList<>();
+        this.listaTempArbitros = new ArrayList<>();
+    }
+    public ControladorArbitros(Arbitro arbitro, ArrayList<Arbitro> listaArbitros, ArrayList<Arbitro> listaTempArbitros) {
+        this.listaArbitros = listaArbitros;
+        this.listaTempArbitros = listaTempArbitros;
+    }
+
+    public Arbitro getArbitro() {
+        return arbitro;
+    }
+
+    public ArrayList<Arbitro> getListaArbitros() {
+        return listaArbitros;
+    }
+
+    public ArrayList<Arbitro> getListaTempArbitros() {
+        return listaTempArbitros;
+    }
 
 
+    public void AddArbitro(Arbitro arbitro){
+        this.listaArbitros.add(arbitro);
+    }
+    
+    public void GetArbitroPorNombre(String nombre, String apellido){
+        this.listaTempArbitros.clear();
+        for(Arbitro i : this.listaArbitros){
+                if(i.getNombre().equals(nombre) && i.getApellido().equals(apellido)){
+                    this.listaTempArbitros.add(i);
+            }
+        }
+    }
+    
+    public Arbitro GetArbitroPorCodigo(int codigo){
+        for(Arbitro i : this.listaArbitros){
+            if(i.getCodigo() == codigo){
+                return i;
+            }
+        }
+        Arbitro arbitroNulo = null;
+        return arbitroNulo;
+    }
+    //Almacena en la lista temporal los arbitros internacionales
+    public void GetArbitrosInternacionales(){
+        this.listaTempArbitros.clear();
+        for(Arbitro i : this.listaArbitros){
+            if(i.isInternacional()){
+                this.listaTempArbitros.add(i);
+            }
+        }
+    }
+    //Almacena en la lista temporal los arbitros Nacionales
+    public void GetArbitrosNacionales(){
+        this.listaTempArbitros.clear();
+        for(Arbitro i : this.listaArbitros){
+            //If negado, osea que no sea internacional
+            if(i.isInternacional()){
+                this.listaTempArbitros.add(i);
+            }
+        }
+    }
+    
+    
 }
