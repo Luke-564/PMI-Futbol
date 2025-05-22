88294371d8c533664cfc4aa9700a78b1c138b32e
Luke-564 <luanabressi564@gmail.com>
2025-05-22 14:58:52 -0300

    Update Persona.java
    Agrega la variable codigo

--- PMI-Futbol/src/Model/Persona.java
+++ PMI-Futbol/src/Model/Persona.java
@@ -6,14 +6,17 @@ public class Persona {
     private String apellido;
     private Fecha nacimiento;
     private String nacionalidad;
+    private int codigo;
     //Constructores
     public Persona() {
     }
-    public Persona(String nombre, String apellido, Fecha nacimiento, String nacionalidad) {
+    public Persona(String nombre, String apellido, Fecha nacimiento, 
+            String nacionalidad, int codigo) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.nacimiento = nacimiento;
         this.nacionalidad = nacionalidad;
+        this.codigo = codigo;
     }
     //Getters / Observadores
     public String getNombre() {
@@ -28,6 +31,9 @@ public class Persona {
     public String getNacionalidad() {
         return nacionalidad;
     }
+    public int getCodigo() {
+        return codigo;
+    }
     //Setters / Modificadores
     public void setNombre(String nombre) {
         this.nombre = nombre;
@@ -41,10 +47,15 @@ public class Persona {
     public void setNacionalidad(String nacionalidad) {
         this.nacionalidad = nacionalidad;
     }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
     //toString, no se si es necesario pero por las dudas
     @Override
     public String toString() {
-        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", nacimiento=" + nacimiento + ", nacionalidad=" + nacionalidad + '}';
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + 
+                ", nacimiento=" + nacimiento + ", nacionalidad=" 
+                + nacionalidad + ", codigo=" + codigo + '}';
     }
-    //Metodos
 }
