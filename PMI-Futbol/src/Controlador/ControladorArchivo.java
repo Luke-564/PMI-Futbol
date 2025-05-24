f1f1830233e8e0a766b8a1b9edae879af2af06cd
ucrm36 <148591700+ucrm36@users.noreply.github.com>
2025-05-24 10:46:52 -0300

    Vista y Controlador
    caca

--- PMI-Futbol/src/Controlador/ControladorArchivo.java
+++ PMI-Futbol/src/Controlador/ControladorArchivo.java
@@ -7,17 +7,4 @@ import java.io.IOException;
 
 public class ControladorArchivo {
     File AJugadoras = new File ("\\PMI-Futbol\\PMI-FutbolJugadoras.txt");
-    try{
-    if(AJugadoras.createNewFile("\\PMI-Futbol\\PMI-FutbolJugadoras.txt")){
-    System.out.println("creado");
-}
-    else{
-    System.out.println("nooo la po");
-}
-    
-    }
-    catch(IOException e){
-    System.out.println("nooo");
-    e.printStackTrace();
-    }
 }
